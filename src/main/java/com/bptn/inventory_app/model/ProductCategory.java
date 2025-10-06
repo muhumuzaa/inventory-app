6574dc7ec9fed0a51d107e49760f0f5315ce65fa
Ambroze AK <muhumuzaamb@gmail.com>
2025-10-06 15:38:54 -0400

    Add App and ProductCategory classes

---
+++ src/main/java/com/bptn/inventory_app/model/ProductCategory.java
@@ -0,0 +1,12 @@
+package com.bptn.inventory_app.model;
+
+
+/**
+* Enumeration representing the different categories a product can belong to.
+* This helps in organizing and filtering products within the inventory system.
+*/
+
+
+public enum ProductCategory {
+   ELECTRONICS, BOOKS, CLOTHING, GROCERIES, HOME_GOODS, TOYS, SPORTS, BEAUTY, OTHER
+}
